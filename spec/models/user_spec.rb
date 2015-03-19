dd542fa6ee9332ff41953e8de48195110d0710d9
zz <ziyuan.zhou@10020.cn>
2015-03-19 16:09:58 +0800

    Make a basic User model

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -63,6 +63,15 @@ describe User do
 		it { should_not be_valid }
 	end
 
+	describe "email address with mixed case" do
+		let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+		it "should be saved as all lower-case" do
+			@user.email = mixed_case_email
+			@user.save
+			@user.reload.email.should == mixed_case_email.downcase
+		end
+	end
+
 	describe "when password is not present" do
 		before { @user.password = @user.password_confirmation = " " }
 		it { should_not be_valid }
@@ -88,13 +97,13 @@ describe User do
 		let(:found_user) { User.find_by_email(@user.email) }
 
 		describe "with valid password" do
-			before { should == found_user.authenticate(@user.password) }
+			it { should == found_user.authenticate(@user.password) }
 		end
 
 		describe "with invalid password" do
 			let(:user_for_invalid_password) { found_user.authenticate("invalid") }
 			it { should_not == user_for_invalid_password }
-			specify { user_for_invalid_password.should be_false }
+			#specify { user_for_invalid_password.should be_false }
 		end
 	end                                                                                                                                                                                                                                                                                                                                                                                                                           
 end
